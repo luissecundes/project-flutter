9b179e192540f68c361dcdff60a597e98f873dc3
Luis Henrique <luissecundes@gmail.com>
2024-10-02 09:40:46 -0300

    alterada route inicial

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
         '/': (context) => LoginScreen(),
         '/home': (context) => const HomeScreen(),
