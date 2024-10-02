ee09c43e72bef11a14a7366d816b149169f5a296
Luis Henrique <luissecundes@gmail.com>
2024-10-02 00:54:28 -0300

    alteração no background color

--- lib/screens/login/custom_elevated_button.dart
+++ lib/screens/login/custom_elevated_button.dart
@@ -13,7 +13,10 @@ class CustomElevatedButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blueAccent, // Altere 'primary' para 'backgroundColor'
+        backgroundColor:
+            Colors.blueAccent, // Altere 'primary' para 'backgroundColor'
+        foregroundColor: Colors.white, // Cor do texto do botão
+
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
