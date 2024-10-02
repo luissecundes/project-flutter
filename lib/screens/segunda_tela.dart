5165600aa282c9fcb28eeb9f8bee7ec039a40b23
Luis Henrique <luissecundes@gmail.com>
2024-10-02 00:22:43 -0300

    criada segunda tela aleatória

---
+++ lib/screens/segunda_tela.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SegundaTela extends StatelessWidget {
+  const SegundaTela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Segunda Tela'),
+      ),
+      body: Center(
+        child: const Text('Bem-vindo à Segunda Tela!'),
+      ),
+    );
+  }
+}
